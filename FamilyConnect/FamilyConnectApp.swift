ccdcb26971e12dcedc7a46bfd2b0b102bf4e68a3
Garrison Blair <garrisonblair96@gmail.com>
2020-09-30 10:36:26 +0200

    Initial Commit

---
+++ FamilyConnect/FamilyConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyConnectApp.swift
+//  FamilyConnect
+//
+//  Created by Garrison Blair on 2020-09-30.
+//
+
+import SwiftUI
+
+@main
+struct FamilyConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
