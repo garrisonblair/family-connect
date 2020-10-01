713d8cf5d65e50d8330fc7bc40e61ce72ba6d4bd
Garrison Blair <garrisonblair96@gmail.com>
2020-10-01 10:25:21 +0200

    add flexibility to threshold

--- FamilyConnect/Supporting Views/RatingView.swift
+++ FamilyConnect/Supporting Views/RatingView.swift
@@ -9,14 +9,24 @@ import SwiftUI
 
 struct RatingView: View {
     let rating: Double
+    var threshold: Double = 0.8
+    
+    init(rating: Double) {
+        self.rating = rating
+    }
+    
+    init(rating: Double, threshold: Double) {
+        self.rating = rating
+        self.threshold = (0...1).contains(threshold) ? threshold : 0.8
+    }
     
     var body: some View {
         HStack {
-            Image(systemName: rating > 0.8 ? "heart.fill" : "heart")
-            Image(systemName: rating > 1.8 ? "heart.fill" : "heart")
-            Image(systemName: rating > 2.8 ? "heart.fill" : "heart")
-            Image(systemName: rating > 3.8 ? "heart.fill" : "heart")
-            Image(systemName: rating > 4.8 ? "heart.fill" : "heart")
+            Image(systemName: rating > threshold ? "heart.fill" : "heart")
+            Image(systemName: rating > 1 + threshold ? "heart.fill" : "heart")
+            Image(systemName: rating > 2 + threshold ? "heart.fill" : "heart")
+            Image(systemName: rating > 3 + threshold ? "heart.fill" : "heart")
+            Image(systemName: rating > 4 + threshold ? "heart.fill" : "heart")
         }
         .foregroundColor(Color("appOrange"))
     }
@@ -24,6 +34,6 @@ struct RatingView: View {
 
 struct RatingView_Previews: PreviewProvider {
     static var previews: some View {
-        RatingView(rating: 3.9)
+        RatingView(rating: 3.4, threshold: 0.4)
     }
 }
