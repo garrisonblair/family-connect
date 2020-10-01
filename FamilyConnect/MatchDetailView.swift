c169026e586710fc22846b12bbe120f6562ba330
Garrison Blair <garrisonblair96@gmail.com>
2020-10-01 09:36:37 +0200

    created stub for matchDetailView

---
+++ FamilyConnect/MatchDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  MatchDetailView.swift
+//  FamilyConnect
+//
+//  Created by Garrison Blair on 2020-10-01.
+//
+
+import SwiftUI
+
+struct MatchDetailView: View {
+    let match: Family
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatchDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchDetailView(match: families[0])
+    }
+}
