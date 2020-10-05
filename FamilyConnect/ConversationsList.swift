923d615ab6df0ba788ebf2c881b6c882a6e0b26a
Garrison Blair <garrisonblair96@gmail.com>
2020-10-05 09:57:24 +0200

    import conversation view

--- FamilyConnect/ConversationsList.swift
+++ FamilyConnect/ConversationsList.swift
@@ -1,10 +1,3 @@
-//
-//  ConversationsList.swift
-//  FamilyConnect
-//
-//  Created by Garrison Blair on 2020-10-01.
-//
-
 import SwiftUI
 
 struct ConversationsList: View {
--- FamilyConnect/Supporting Views/Chat/ConversationRow.swift
+++ FamilyConnect/Supporting Views/Chat/ConversationRow.swift
@@ -1,10 +1,3 @@
-//
-//  ConversationRow.swift
-//  FamilyConnect
-//
-//  Created by Garrison Blair on 2020-10-01.
-//
-
 import SwiftUI
 
 struct ConversationRow: View {
