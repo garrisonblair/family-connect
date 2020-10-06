5a251d57d88cb1c93b7dbae1542d1cbb169fc094
Garrison Blair <garrisonblair96@gmail.com>
2020-10-06 11:44:28 +0200

    Display comments in sheet

--- FamilyConnect/Supporting Views/Matches/MatchProfileView.swift
+++ FamilyConnect/Supporting Views/Matches/MatchProfileView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct MatchProfileView: View {
+    @State var showComments: Bool = false
+    
     let profile: Profile
     
     var body: some View {
@@ -17,10 +19,47 @@ struct MatchProfileView: View {
                         .padding(.top)
                     Divider()
                         .background(Color("appBackground"))
+                    Text("Les avis")
+                        .font(.title)
+                        .fontWeight(.bold)
                     if profile.ratings.count > 0 {
+                        if profile.ratings[0].comment != "" {
+                            RatingCommentRow(rating: profile.ratings[0])
+                                .framedStyle()
+                                .padding(.top)
+                        }
+                        if profile.ratings.count > 1 {
+                            if profile.ratings[1].comment != "" {
+                                RatingCommentRow(rating: profile.ratings[1])
+                                    .framedStyle()
+                                    .padding(.top)
+                            }
+                        }
+                        Button(action: {self.showComments.toggle()}, label: {
+                            Text("Voir tous les avis")
+                                .foregroundColor(Color("appOrange"))
+                                .padding()
+                        })
+                    } else {
+                        Text("Il n'y a pas d'avis a ce moment")
+                    }
+                })
+            }
+            .sheet(isPresented: $showComments, content: {
+                VStack {
+                    HStack {
                         Text("Les avis")
                             .font(.title)
                             .fontWeight(.bold)
+                        Spacer()
+                        Button(action: {self.showComments.toggle()}, label: {
+                            Text("Terminé")
+                                .foregroundColor(Color("appOrange"))
+                        })
+                    }
+                    .padding()
+                    Divider()
+                    ScrollView {
                         ForEach(0...profile.ratings.count-1, id: \.self) { count in
                             if profile.ratings[count].comment != "" {
                                 RatingCommentRow(rating: profile.ratings[count])
@@ -29,8 +68,10 @@ struct MatchProfileView: View {
                             }
                         }
                     }
-                })
-            }
+                    Spacer()
+                }
+                .padding(.horizontal)
+            })
         }
         .padding(.horizontal)
     }
@@ -38,7 +79,7 @@ struct MatchProfileView: View {
 
 extension View {
     func framedStyle(color: Color = Color("appBackground")) -> some View {
-        self.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        self.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0)
             .padding(.all)
             .background(color)
             .cornerRadius(20.0)
